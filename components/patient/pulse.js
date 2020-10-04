8cd194ac8598db62877f9e108f48512b2b655f9c
a-x-hn <alexhan0324@gmail.com>
2020-10-04 01:45:46 -0700

    all skeleton done

--- components/patient/pulse.js
+++ components/patient/pulse.js
@@ -3,16 +3,18 @@ import { Component } from 'react';
 import { StyleSheet, ScrollView, ActivityIndicator, FlatList, Text, View } from 'react-native';
 import 'react-native-gesture-handler';
 
+import PulseGraph from './pulseutils/pulsegraph'
+
 export default function Pulse({ data }) {
     return (
-        <View>
-            <Text>Pulse Measure</Text>
-            <Text>Last Updated</Text>
-            <ScrollView>
-                <Text>Current</Text>
-                <Text>Last Week</Text>
-                <Text>Last Month</Text>
-            </ScrollView>
+        <View style={styles.main}>
+            <Text style={styles.heading}>Pulse Measure</Text>
+            <Text style={styles.lastUpdated}>Last Updated</Text>
+            <View style={styles.setting}>
+                <Text style={styles.btn}>Current</Text>
+                <Text style={styles.btn}>Last Week</Text>
+                <Text style={styles.btn}>Last Month</Text>
+            </View>
             <View>
                 <Text>Graph</Text>
             </View>
@@ -21,5 +23,25 @@ export default function Pulse({ data }) {
 }
 
 const styles = StyleSheet.create({
-
+    main: {
+        backgroundColor: 'skyblue',
+        padding: 20,
+        marginBottom: 10
+    },
+    heading: {
+        fontSize: 25,
+        fontWeight: 'bold',
+    },
+    lastUpdated: {
+        fontSize: 10,
+        paddingBottom: 10
+    },
+    setting: {
+        flexDirection: 'row',
+    },
+    btn: {
+        padding: 10,
+        color: 'white',
+        backgroundColor: 'turquoise',
+    }
   });
