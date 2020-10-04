548f951c2713086febfbac4e7fcf0fedd0b07b46
a-x-hn <alexhan0324@gmail.com>
2020-10-04 01:38:18 -0700

    deprecated heartgraph for pulsegraph

--- components/patient/pulse.js
+++ components/patient/pulse.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import { Component } from 'react';
-import { StyleSheet, ActivityIndicator, FlatList, Text, View } from 'react-native';
+import { StyleSheet, ScrollView, ActivityIndicator, FlatList, Text, View } from 'react-native';
 import 'react-native-gesture-handler';
 
 export default function Pulse({ data }) {
     return (
         <View>
-            <Text>Pulse</Text>
+            <Text>Pulse Measure</Text>
+            <Text>Last Updated</Text>
+            <ScrollView>
+                <Text>Current</Text>
+                <Text>Last Week</Text>
+                <Text>Last Month</Text>
+            </ScrollView>
+            <View>
+                <Text>Graph</Text>
+            </View>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+
+  });
